0bb574ec943a5abeae6c9c276445c49c202e2ef2
thecafe75000 <hualei3375@gmail.com>
2024-02-06 20:39:32 +0100

    initialize the data of todolist using useState and useEffect

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '@/components/Header'
 import List from '@/components/List'
 import Footer from '@/components/Footer'
@@ -6,7 +6,18 @@ import './App.less'
 import { reqTodolist } from '@/api'
 
 function App() {
- reqTodolist()
+  //? Initialize a state specifically designed to store the list data retrieved from requests
+  const [todoList, setTodoList] = useState([])
+
+  //? Send an Ajax request to fetch initialization data, this operation must be completed within the useEffect
+  useEffect(() => {
+    const getTodolist = async () => {
+      const result = await reqTodolist()
+      console.log(result.data)
+    }
+    getTodolist()
+  }, [])
+
   return (
     <div className='todo-container'>
       <div className='todo-wrap'>
